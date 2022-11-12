59cc29257a735d147f82c44536d251e466c5e6a8
DsanchezV1 <116523413+DsanchezV1@users.noreply.github.com>
2022-11-12 14:42:07 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.principal;
 
-/**
- *
- * @author clase
- */
+
 import java.io.*;
 import java.util.ArrayList;
 public class Vehiculo {
