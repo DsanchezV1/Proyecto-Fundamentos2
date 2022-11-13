91ed76e24bafb0c4f0038d231453258ca986fb0d
DsanchezV1 <116523413+DsanchezV1@users.noreply.github.com>
2022-11-12 21:52:21 -0500

    Update and rename Principal.java to PrincipalVehiculosYSensores.java

--- PrincipalVehiculosYSensores.java
+++ PrincipalVehiculosYSensores.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.principal;
 
-/**
- *
- * @author clase
- */
+
 import java.util.Scanner;
 public class Principal {
     public static void main(String[] args){
